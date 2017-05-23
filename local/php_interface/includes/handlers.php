c7ff27c5bf1b291948efcd5635f1d97ed8815f88
anton <anton@ms-net.ru>
2017-05-23 17:55:28 +0300

    f

--- local/php_interface/includes/handlers.php
+++ local/php_interface/includes/handlers.php
@@ -1,10 +1,1 @@
 <?php
-
-function setSEO($title, $description, $image, $url) {
-    global $APPLICATION;
-    $host = "//" . SITE_SERVER_NAME;
-    $APPLICATION->SetPageProperty("og_title", strip_tags(htmlspecialchars_decode($title)));
-    $APPLICATION->SetPageProperty("og_description", strip_tags(htmlspecialchars_decode($description)));
-    $APPLICATION->SetPageProperty("og_image", $host . $image);
-    $APPLICATION->SetPageProperty("og_url", $host . $url);
-}
