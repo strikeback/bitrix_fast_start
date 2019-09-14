5a1772fc0db6d70f9c54dec3d4b9ad4bca62d6a8
anton <anton@ms-net.ru>
2019-09-14 11:37:45 +0300

    disabled sanitize html

--- _include/for_all_pages.php
+++ _include/for_all_pages.php
@@ -4,14 +4,12 @@ use Bitrix\Main\Page\Asset;
 ?>
 <? Asset::getInstance()->addJs('/_plugins/jquery-3.4.1.min.js'); ?>
 
-<? $APPLICATION->SetAdditionalCSS('/_plugins/bootstrap/css/bootstrap.min.css'); ?>
-<? Asset::getInstance()->addJs('/_plugins/bootstrap/js/bootstrap.min.js'); ?>
 
 <? $APPLICATION->SetAdditionalCSS('/_plugins/slick/slick.css'); ?>
 <? Asset::getInstance()->addJs('/_plugins/slick/slick.js'); ?>
 
-<? $APPLICATION->SetAdditionalCSS('/_plugins/scrollbar/jquery.mCustomScrollbar.css'); ?> 
-<? Asset::getInstance()->addJs('/_plugins/scrollbar/jquery.mCustomScrollbar.concat.min.js'); ?>
+<? // $APPLICATION->SetAdditionalCSS('/_plugins/scrollbar/jquery.mCustomScrollbar.css'); ?> 
+<? // Asset::getInstance()->addJs('/_plugins/scrollbar/jquery.mCustomScrollbar.concat.min.js'); ?>
 
 <? Asset::getInstance()->addJs('/_plugins/mask/jquery.maskedinput.min.js'); ?>
 
