5416595cf80f081415d87ac11d684cc09fb33cb3
anton <anton@ms-net.ru>
2020-02-03 15:00:24 +0300

    fixes

---
+++ _include/footer.php
@@ -0,0 +1,20 @@
+<? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die(); ?>
+
+</div>
+<div class="layout__footer">
+
+</div> <!-- laout__footer -->
+</div> <!-- laout -->
+
+
+<?
+$APPLICATION->IncludeComponent("bitrix:main.include", "", Array(
+    "AREA_FILE_SHOW" => "file",
+    "PATH" => "/_include/metrics.php",
+    "AREA_FILE_SUFFIX" => "inc",
+    "AREA_FILE_RECURSIVE" => "N",
+    "EDIT_TEMPLATE" => ""
+        )
+);
+?>
+<a href="javascript:void(0);" class="go_top js-go_top hidden_bt"></a>
